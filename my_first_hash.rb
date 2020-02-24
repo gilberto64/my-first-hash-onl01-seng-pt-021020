937eb13f1b2b217ea3bc76b65360c2b67918888e
Gilberto Ocampo <ocampo@gate.net>
2020-02-24 02:55:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   
-  my_name 
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+  my_hash = {"my_first_name" => "Gilberto", "my_last_name" => "Ocampo"}
+  
 
   
 end
